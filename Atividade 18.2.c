b6b690e96be8d4f6ea8d9ee6ef7eb0cafcb4bff8
Teixluca <130296558+Teixluca@users.noreply.github.com>
2024-08-31 20:47:48 -0300

    Add files via upload

---
+++ Atividade 18.2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/*2. Desenvolva um programa que imprima as seguintes seus dados pessoais a seguir:
+Nome completo, idade, endereço, bairro, cidade e estado.*/
+int main ()
+{
+    printf("\n       INFORME SEUS DADOS      \n");
+    printf("\nNome Completo: Lucas Alves teixeira \n");
+    printf("Idade: 30\n");
+    printf("Endereco: Rua Dr. Levi Miro Carneiro, 37 \n");
+    printf("Cidade: Curitiba \n");
+    printf("Estado: PR\n");
+    return 0;
+
+}
